d1b1d1fb6324c98fd110f898e3255706a628fc23
Alexey <alexey.kostenyu@gmail.com>
2023-06-16 22:52:47 +0400

    1 commit - working venv with all google-api installed

--- homework-1/src/channel.py
+++ homework-1/src/channel.py
@@ -1,12 +1,13 @@
-
+import json
+import os
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(channel_id)
