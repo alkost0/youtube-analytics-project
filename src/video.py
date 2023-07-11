1b0d07e604c598501212301b6a7b294be53bbca0
Alexey <alexey.kostenyu@gmail.com>
2023-07-11 11:08:54 +0400

    7 commit - add exceptions on init video-class in Homework6(16.1)

--- src/video.py
+++ src/video.py
@@ -4,11 +4,18 @@ class Video(Channel):
 
     def __init__(self, id_video):
         self.__id_video = id_video
-        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
-        self.title = str(video_response['items'][0]['snippet']['title'])
-        self.url = f"https://youtu.be/{self.__id_video}"
-        self.count_of_views = int(video_response['items'][0]['statistics']['viewCount'])
-        self.count_of_likes = int(video_response['items'][0]['statistics']['likeCount'])
+        try:
+            video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
+            self.title = str(video_response['items'][0]['snippet']['title'])
+            self.url = f"https://youtu.be/{self.__id_video}"
+            self.count_of_views = int(video_response['items'][0]['statistics']['viewCount'])
+            self.count_of_likes = int(video_response['items'][0]['statistics']['likeCount'])
+        except Exception:
+            self.title = None
+            self.url = None
+            self.count_of_views = None
+            self.count_of_likes = None
+
 
     def __str__(self):
         return f"{self.title}"
